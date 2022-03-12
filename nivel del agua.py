485649a213c65f26dc2af769179c32492de1b9f7
cuervob <101007906+cuervob@users.noreply.github.com>
2022-03-12 15:09:37 -0600

    Add files via upload

---
+++ nivel del agua.py
@@ -0,0 +1,20 @@
+#entrada
+metros=float(input("metros de agua: "))
+
+#proceso
+if (metros < 0):
+    print("FUGA DE AGUA")
+elif (metros == 0):
+    print("ENCENDER BOMBA")
+elif (metros >0 and metros <2):
+    print("ACELEREAR BOMBA DE AGUA")
+elif (metros >=2 and metros <4):
+    print("BOMBA TRABAJANDO")
+elif (metros >=4 and metros <6):
+    print("DESACELERAR BOMBA")
+elif (metros == 6):
+    print("APAGAR BOMBA")
+elif (metros>6):
+    print("DESBORDAMINETO DE AGUA EN CISTERNA")
+else: 
+    print("ERROR")
